022968f485f3157c1901df5bc7b2ae65f29aa392
matheus rosa <matheus.rosa27@escola.pr.gov.br>
2025-06-30 10:57:01 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+  'Programação',
+  'O que é Phyton?',
+  'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+  'Geografia',
+  'Qual a capital da França?',
+  'A capital da França é Paris'
+)
+
+criaCartao(
+  'Programação',
+  'O que é uma função?',
+  'Função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+  'Lingua inglesa',
+  'Como se diz oi em inglês?',
+  'Oi se diz HI em inglês'
+)
+
+criaCartao(
+  'Geografia',
+  'em que país moramos?',
+  'Brasil'
+)
+
+criaCartao(
+  'Geografia',
+  'Qual capital do Paraná?',
+  'É curitiba'
+)
